d28ba5393c06cabeff8989fa8d7aafdd1c2f8f2d
Dhruba2004 <dhrubaduttabanik8@gmail.com>
2024-09-26 22:11:14 +0530

    fix  :  updated tsconfig

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,25 +2,40 @@ import Link from "next/link";
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
 import { Button } from "./ui/moving-border";
+import Spline from "@splinetool/react-spline";
 
 function HeroSection() {
   return (
     <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
-      <div className="p-4 relative z-10 w-full text-center">
-      <Spotlight
-        className="-top-40 left-0 md:left-60 md:-top-20"
-        fill="white"
-      />
-        <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">Master the art of music</h1>
-        <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
-          Dive into our comprehensive music courses and transform your musical
-          journey today. Whether you are a beginner or looking to refine your
-          skills, join us to unlock your true potential.
-        </p>
-        <div className="mt-4">
-          <Link href="/courses">
-            <Button borderRadius="1.75rem" className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800">Explore courses</Button>
-          </Link>
+      <div className="container grid grid-cols-1 md:grid-cols-2">
+        <div className="p-4 relative z-10 w-full text-center mt-[12rem]">
+          <Spotlight
+            className="-top-40 left-0 md:left-60 md:-top-20"
+            fill="white"
+          />
+
+          <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+            Master the art of music
+          </h1>
+          <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
+            Dive into our comprehensive music courses and transform your musical
+            journey today. Whether you are a beginner or looking to refine your
+            skills, join us to unlock your true potential.
+          </p>
+
+          <div className="mt-4">
+            <Link href="/courses">
+              <Button
+                borderRadius="1.75rem"
+                className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+              >
+                Explore courses
+              </Button>
+            </Link>
+          </div>
+        </div>
+        <div className="h-screen">
+          <Spline scene="https://prod.spline.design/Lyw0DkVocEUFPAoa/scene.splinecode" />
         </div>
       </div>
     </div>
